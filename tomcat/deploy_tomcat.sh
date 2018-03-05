a1cdfdb88e8bdfc61d23f52e749a5f161398cf24
shdkej <shdkej@naver.com>
2018-03-05 16:30:41 +0900

    docker update 18/03/05
    Former-commit-id: cf04c6c38eb3d18af53494fbd4824c4b849b8416

--- tomcat/deploy_tomcat.sh
+++ tomcat/deploy_tomcat.sh
@@ -5,10 +5,11 @@ $USER = gvm
 cd /home/$USER/shdkej/board
 ./mvnw clean package
 cp target/gvm.war /home/$USER/docker/tomcat/gvm.war
-docker stop tomcat
-docker rm tomcat
+docker stop docker_tomcat_1
+docker rm docker_tomcat_1
 
 cd /home/$USER/docker/tomcat/
-docker build -t tomcat .
-docker run -d -p 90:8080 --name tomcat tomcat
-docker restart tomcat
+#docker build -t tomcat .
+#docker run -d -p 90:8080 --name tomcat tomcat
+docker-compose up -d --build tomcat
+docker restart docker_tomcat_1
